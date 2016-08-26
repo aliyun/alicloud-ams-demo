20530531022915129cd37ea67b58359225d5a3e4
许历 <lingbo.zlb@alibaba-inc.com>
2016-08-26 14:40:51 +0800

    php tag update

---
+++ push-openapi-php-demo/UnbindTag.php
@@ -0,0 +1,24 @@
+<?php
+
+include_once '../aliyun-php-sdk-core/Config.php';
+use \Push\Request\V20150827 as Push;
+
+$accessKeyId = "";
+$accessSecret = "";
+$appKey = 123456;
+$deviceId = '';
+$account = '';
+$iClientProfile = DefaultProfile::getProfile("cn-hangzhou", $accessKeyId, $accessSecret);
+$client = new DefaultAcsClient($iClientProfile);
+$request = new Push\UnbindTagRequest();
+
+$request->setAppKey($appKey);
+$request->setKeyType(1);//1: device 2: account
+$request->setClientKey($deviceId);
+$request->setTagName("tag2");
+
+$response = $client->getAcsResponse($request);
+print_r("\r\n");
+print_r($response);
+
+?>
