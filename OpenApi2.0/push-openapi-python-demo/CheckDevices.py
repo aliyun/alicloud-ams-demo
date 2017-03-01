54e27ebb53c266b1f5bfd8d976d52d5f8ccc7d00
许历 <lingbo.zlb@alibaba-inc.com>
2017-03-01 10:40:42 +0800

    Udpdate

--- OpenApi2.0/push-openapi-python-demo/CheckDevices.py
+++ OpenApi2.0/push-openapi-python-demo/CheckDevices.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python
 #coding=utf-8
 import properties
-from aliyunsdkpush.request.v20160801 import CheckDeviceRequest
+from aliyunsdkpush.request.v20160801 import CheckDevicesRequest
 from aliyunsdkcore import client
 
 clt = client.AcsClient(properties.accessKeyId,properties.accessKeySecret,properties.regionId)
 
-request = CheckDeviceRequest.CheckDeviceRequest()
+request = CheckDevicesRequest.CheckDevicesRequest()
 request.set_AppKey(properties.appKey)
 request.set_DeviceId(properties.deviceIds)
 
